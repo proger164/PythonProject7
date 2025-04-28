aafb6c3a9d8f38fab557f6c5d5c7f06daafe8c6b
proger164 <gerasimenkosv@bk.ru>
2025-04-28 21:44:26 +0300

    Выводим сообщение

--- blake.py
+++ blake.py
@@ -1,10 +1,11 @@
-import os
-import math
-from hashlib import blake2b
-msg = b'some message'
-# Вычислите первый хеш со случайной солью.
-salt1 = os.urandom(blake2b.SALT_SIZE)
-h1 = blake2b(salt=salt1)
+print(111)
+# import os
+# import math
+# from hashlib import blake2b
+# msg = b'some message'
+# # Вычислите первый хеш со случайной солью.
+# salt1 = os.urandom(blake2b.SALT_SIZE)
+# h1 = blake2b(salt=salt1)
 # h1.update(msg)
-
-# True
+#
+# # True
